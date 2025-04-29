137ffb243a30c7812875990cf73fe2b2b2baa124
Jaswant10041 <jaswanthkumarmadineni@gmail.com>
2025-04-29 10:49:04 +0530

    latest9

--- src/CardDetails.jsx
+++ src/CardDetails.jsx
@@ -6,7 +6,7 @@ const CardDetails = ({ data }) => {
   return (
     <div className="w-full sm:w-80 md:w-96 mx-auto p-6 bg-white rounded-2xl shadow-md border border-gray-200 hover:shadow-xl transition-all duration-300">
       <h2 className="text-xl font-bold text-gray-800 mb-2">{data.title}</h2>
-      <p className="text-xs text-gray-400 mb-4">gosted on: {new Date(data.reported_at).toLocaleDateString()}</p>
+      <p className="text-xs text-gray-400 mb-4 font-bold">Posted on: {new Date(data.reported_at).toLocaleDateString()}</p>
 
       {showDetails ? (
         <p className="text-gray-700 text-sm">{data.description}</p>
